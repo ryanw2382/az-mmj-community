94d593b26586917013a8be9083011aaacc306b5f
Ryan Wilson <ryanw2382@gmail.com>
2017-10-11 15:13:20 -0700

    copied code from the About container just so it wouldnt totally error out

---
+++ src/containers/Flowers/Flowers.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import { injectIntl, intlShape } from 'react-intl';
+import { GitHubIcon } from '../../components/Icons';
+import { Activity } from '../../containers/Activity';
+import  ReactMarkdown from 'react-markdown'
+import Scrollbar from '../../components/Scrollbar/Scrollbar'
+import README from './README.md';
+
+require('github-markdown-css');
+
+class Flowers extends Component {
+
+  //Sorry for using setState here but I have to remove 'marked' from the dependencies
+  //because of a vulnerability issue
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+  }
+
+  componentWillMount(){
+
+    fetch(README)
+    .then(response => response.text())
+    .then(text => {
+      this.setState({text:text})
+    });
+  }
+
+  render() {
+    const { intl }= this.props
+
+    return (
+        <Activity
+            iconElementRight={
+              <FlatButton
+                  style={{marginTop: 4}}
+                  href="https://github.com/ryanw2382/az-mmj-community"
+                  target="_blank"
+                  rel="noopener"
+                  secondary={true}
+                  icon={<GitHubIcon/>}
+              />
+            }
+            title={intl.formatMessage({id: 'flowers'})}>
+
+          <Scrollbar>
+            <div style={{backgroundColor: 'white', padding: 5}}>
+              <ReactMarkdown
+                  className="markdown-body"
+                  source={this.state.text}
+              />
+            </div>
+          </Scrollbar>
+
+        </Activity>
+    );
+
+  }
+}
+
+Flowers.propTypes = {
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(Flowers);

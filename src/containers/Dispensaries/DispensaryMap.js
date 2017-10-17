757022ff29384f4ef51e544c218dc1e4c55ce780
Ryan Wilson <ryanw2382@gmail.com>
2017-10-16 21:01:03 -0700

    still working on it

--- src/containers/Dispensaries/DispensaryMap.js
+++ src/containers/Dispensaries/DispensaryMap.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react'
 import { Activity } from '../../containers/Activity'
 
 class DispensaryMap extends Component {
-      render(){
 
-        return(
-            <Activity title="Dispensary Map">
-              <div className="google-maps">
-                <iframe title="Dispensary Map" src="https://www.google.com/maps/d/u/0/embed?mid=1bHhT2nZ6EkRParxJSGN3ULfYr-o" width="600" height="450" />
-              </div>
-            </Activity>
+  render() {
 
-          )
+    return (
+      <Activity title="Dispensary Map">
+        <div className="google-maps">
+          <iframe title="Dispensary Map" src="https://www.google.com/maps/d/u/0/embed?mid=1bHhT2nZ6EkRParxJSGN3ULfYr-o" />
+        </div>
+      </Activity>
 
+    )
   }
 }
 

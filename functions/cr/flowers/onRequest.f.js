0d149a3f2fff9f09c9ac3f574445137fff1ac3b7
Ryan Wilson <ryanw2382@gmail.com>
2017-10-16 21:06:57 -0700

    another attempt at inserting information from the cr api

--- functions/cr/flowers/onRequest.f.js
+++ functions/cr/flowers/onRequest.f.js
@@ -1,21 +1,17 @@
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 try {admin.initializeApp(functions.config().firebase);} catch(e) {} // You do that because the admin SDK can only be initialized once.
-const { setCannabisReportsKey, Flower } = require("cannabis-reports");
+const { setCannabisReportsKey, Flower } = require('cannabis-reports')
 setCannabisReportsKey('9687a012203c12f7556d4a87b1b05294d49ebeb9')
 
+exports.getFlower = functions.https.onRequest((req, res) => {
 
-exports.fetchFlower = functions.https.onRequest((req, res) => {
 
   return Flower
-  .all(options)
-    .then(data => {
-      admin.database().ref('flowers').push({original: original}).then(snapshot => {
-        res.redirect(303, snapshot.ref)
-      });
-  })
+    .all(options)
+    .then(data =>
+      admin.collection('flowers').document('{ucpc}')
+      .add(data))
     .catch(err => console.log(err))
-})
 
-
+});

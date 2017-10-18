d2d946193afeda20a293a26e19df310a99f65a82
Ryan Wilson <ryanw2382@gmail.com>
2017-10-18 09:26:05 -0700

    changed to use yarn

--- before_install.sh
+++ before_install.sh
@@ -2,9 +2,9 @@
 
 #run only on master
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-   npm install -g firebase-tools
-   npm install codecov.io coveralls
+   yarn install -g firebase-tools
+   yarn install codecov.io coveralls
    cd functions
-   npm install
+   yarn install
    cd ..
 fi

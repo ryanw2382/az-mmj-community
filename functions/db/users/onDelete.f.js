62319397559031a53c1a297c9167d60e8e4cd2ed
Ryan Wilson <ryanw2382@gmail.com>
2017-10-17 22:17:29 -0700

    this is working as its supposed to.. except it is not updating the providerId because there is none being written in the first place.

--- functions/db/users/onDelete.f.js
+++ functions/db/users/onDelete.f.js
@@ -7,13 +7,13 @@ exports = module.exports = functions.database.ref('/users/{userUid}').onDelete(
   (event) => {
 
     const user = event.data // The Firebase user.
-    const provider = user.providerData?user.providerData[0]:{}
-    const providerId = provider.providerId?provider.providerId.replace('.com',''):provider.providerId;
+    const provider=user.providerData?user.providerData[0]:{}
+    const providerId=provider.providerId?provider.providerId.replace('.com',''):provider.providerId;
 
     let providerCount = null
 
     if(providerId){
-      providerCount = admin.database()
+      providerCount= admin.database()
       .ref(`/provider_count/${providerId}`)
       .transaction(current => (current || 0) - 1)
     }

700b5d4a1a940d14d8eb03b7f713fb39b2ae3e51
Ryan Wilson <ryanw2382@gmail.com>
2017-10-17 19:28:00 -0700

    files contained within store, updated spacing and syntax

--- src/store/persistentValues/reducer.js
+++ src/store/persistentValues/reducer.js
@@ -4,9 +4,9 @@ export default function persistentValues(state={}, action){
 
   switch (action.type) {
     case types.ON_PERSISTENT_VALUE_CHANGED:
-    return {...state, [action.id]: action.value};
+      return {...state, [action.id]: action.value};
     default:
-    return state;
+      return state;
   }
 
 }

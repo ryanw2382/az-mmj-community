df20f8de9620ccd0917e8c7726a9f734fec5e154
Ryan Wilson <ryanw2382@gmail.com>
2017-10-20 16:40:30 -0700

    upgraded from 4.5.0 to 4.5.1 on the firebase scripts

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.5.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.5.1/firebase-messaging.js');
 
 firebase.initializeApp({
   'messagingSenderId': '909985495567'
